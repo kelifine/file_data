05a061297b16edf870c1e3321d286ad9daab1494
Keli Fine <kelfin373@gmail.com>
2017-03-22 16:18:36 +0000

    first answer

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.post('/file-upload', upload.single('file'), function (req, res, next) {
+    console.log(req.file);
+    var size = req.file.size;
+    console.log(size);
+    res.send({size});
+});
+
+
+app.listen(process.env.PORT||8080);
